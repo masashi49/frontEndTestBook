94a23f367221a8670921761cab6dda4c364109e8
masashi49 <shisama94@gmail.com>
2023-08-28 09:48:24 +0900

    カバレッジレポート

--- sample/src/06/greetByTime.test.ts
+++ sample/src/06/greetByTime.test.ts
@@ -1,6 +1,19 @@
-import { greetByTime } from "./greetByTime";
+import { greetByTime } from './greetByTime';
 
-describe("greetByTime(", () => {
+// describe('テスト名', () => {
+//   beforeEach(() => {
+//     jest.useFakeTimers();
+//   });
+//   afterEach(() => {
+//     jest.useRealTimers();
+//   });
+//   test('おはようテスト', () => {
+//     jest.setSystemTime(new Date(2023, 4, 20, 11, 23, 49));
+//     expect(greetByTime()).toBe('おはよう');
+//   });
+// });
+
+describe('greetByTime(', () => {
   beforeEach(() => {
     jest.useFakeTimers();
   });
@@ -8,18 +21,18 @@ describe("greetByTime(", () => {
     jest.useRealTimers();
   });
   // (1) 「おはよう」を返す関数
-  test("朝は「おはよう」を返す", () => {
+  test('朝は「おはよう」を返す', () => {
     jest.setSystemTime(new Date(2023, 4, 23, 8, 0, 0));
-    expect(greetByTime()).toBe("おはよう");
+    expect(greetByTime()).toBe('おはよう');
   });
   // (2) 「こんにちは」を返す関数
-  xtest("昼は「こんにちは」を返す", () => {
+  xtest('昼は「こんにちは」を返す', () => {
     jest.setSystemTime(new Date(2023, 4, 23, 14, 0, 0));
-    expect(greetByTime()).toBe("こんにちは");
+    expect(greetByTime()).toBe('こんにちは');
   });
   // (3) 「こんばんは」を返す関数
-  xtest("夜は「こんばんは」を返す", () => {
+  test('夜は「こんばんは」を返す', () => {
     jest.setSystemTime(new Date(2023, 4, 23, 21, 0, 0));
-    expect(greetByTime()).toBe("こんばんは");
+    expect(greetByTime()).toBe('こんばんは');
   });
 });
